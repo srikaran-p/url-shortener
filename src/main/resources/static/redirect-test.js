b530249d12fdcac215d3ade5e373bb23e0d49ece
Srikaran P <srikaran.p@gmail.com>
2025-12-20 22:34:57 +0530

    Add load testing

---
+++ src/main/resources/static/redirect-test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 20,          // virtual users
+    duration: '60s',  // test duration
+};
+
+// k6 run redirect-test.js
+export default function () {
+    const url = 'http://localhost:4000/urls/4d95b5d8';
+
+    const res = http.get(url, {
+        redirects: 0,
+    });
+
+    check(res, {
+        'status is 302': (r) => r.status === 302,
+    });
+
+    sleep(1);
+}
